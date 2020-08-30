e0c97140b24c51279939185129f99002b1995442
ilan274 <ilan.herbach@gmail.com>
2020-08-30 16:43:32 -0300

    http get api request returning object (Map) and inner objects access

--- 02_currency_converter/lib/main.dart
+++ 02_currency_converter/lib/main.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&?key=adeba566&";
 
 void main() async {
   http.Response response = await http.get(request);
-  print(response.body);
+  print(json.decode(response.body)['results']['currencies']['USD']);
+  // Accessing inner objects (Map) results > curr > USD
 
   runApp(MaterialApp(
-    title: 'Initial Setup Title',
-    home: Container(color: Colors.blueAccent)
+      title: 'Initial Setup Title',
+      home: Container(color: Colors.blueAccent)
   ));
 }
