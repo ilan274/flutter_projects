bec320b914066982d6de3833fdaa9c823266d783
ilan274 <ilan.herbach@gmail.com>
2020-08-30 15:17:15 -0300

    Async API request

--- 02_currency_converter/lib/main.dart
+++ 02_currency_converter/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+
+const request = "https://api.hgbrasil.com/finance?format=json&?key=adeba566&";
+
+void main() async {
+  http.Response response = await http.get(request);
+  print(response.body);
+
   runApp(MaterialApp(
     title: 'Initial Setup Title',
     home: Container(color: Colors.blueAccent)
