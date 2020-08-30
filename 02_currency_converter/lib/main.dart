e69ca5861b6143150ad3486d8f1fd988fe1acbda
ilan274 <ilan.herbach@gmail.com>
2020-08-30 16:59:04 -0300

    Creating a Future<Map> async function

--- 02_currency_converter/lib/main.dart
+++ 02_currency_converter/lib/main.dart
@@ -7,12 +7,18 @@ import 'dart:convert';
 const request = "https://api.hgbrasil.com/finance?format=json&?key=adeba566&";
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)['results']['currencies']['USD']);
-  // Accessing inner objects (Map) results > curr > USD
+
+  print(await getData());
+  // Calling a Future<Map> async function
 
   runApp(MaterialApp(
       title: 'Initial Segit addtup Title',
-      home: Container(color: Colors.blueAccent)
-  ));
+      home: Container(color: Colors.blueAccent)));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  // Async api request
+  return json.decode(response.body);
+  // Mapping the return (objectifying
 }
