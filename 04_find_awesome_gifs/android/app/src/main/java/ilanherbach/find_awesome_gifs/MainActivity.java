1b50b5dd7da88fbdba07b2054ebdc76f2e23cfda
ilan274 <ilan.herbach@gmail.com>
2020-09-03 22:02:10 -0300

    Initial Setup

---
+++ 04_find_awesome_gifs/android/app/src/main/java/ilanherbach/find_awesome_gifs/MainActivity.java
@@ -0,0 +1,6 @@
+package ilanherbach.find_awesome_gifs;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ 04_find_awesome_gifs/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:find_awesome_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Awesome Gifs',
+    home: HomePage(),
+  ));
+}
