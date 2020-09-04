8e30b11fdd244c094aa19672dd291d0a180c74df
ilan274 <ilan.herbach@gmail.com>
2020-09-04 08:49:15 -0300

    Get API and response test

--- 04_find_awesome_gifs/lib/ui/home_page.dart
+++ 04_find_awesome_gifs/lib/ui/home_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +8,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null) {
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/trending?api_key=y8UpRrqs2dNmKg8N5f5s2v0tP1LEetzV&limit=25&rating=g');
+    } else {
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/search?api_key=y8UpRrqs2dNmKg8N5f5s2v0tP1LEetzV&q=$_search&limit=20&offset=$_offset&rating=g&lang=en');
+    }
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getGifs().then((value) {
+      print('value, $value');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
