aae85e0f16279a01d24acf0bbea8ab010bdc772c
ilan274 <ilan.herbach@gmail.com>
2020-09-07 16:08:10 -0300

    Initializing database

--- 05_easy_contacts/lib/helpers/contact_helper.dart
+++ 05_easy_contacts/lib/helpers/contact_helper.dart
@@ -1,12 +1,46 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+final String contactTable = 'contactTable';
 final String idColumn = 'idColumn';
 final String nameColumn = 'nameColumn';
 final String emailColumn = 'emailColumn';
 final String phoneColumn = 'phoneColumn';
 final String imgColumn = 'imgColumn';
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  // Class only object instance
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  // internal() constructor  - can only be called from inside the class
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'contacts.db');
+    return await openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute(
+        'CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY,'
+        '$nameColumn TEXT, $emailColumn TEXT, $imgColumn TEXT)',
+      );
+    });
+  }
+}
 
 class Contact {
   int id;
